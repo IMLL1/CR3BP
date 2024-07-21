3eb066530571badf4aec5b3362aff97d84d52fe5
Polaris <imll1@icloud.com>
2024-07-21 00:42:11 -0700

    user-controlled optimizer

--- Code/interactive plot.py
+++ Code/interactive plot.py
@@ -1,23 +1,21 @@
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Button, Slider
+from matplotlib.widgets import Button, Slider, CheckButtons
 
 import numpy as np
 from CR3BP import CR3BP
 
 plt.style.use("dark_background")
 
-obj = CR3BP()
-
-# TODO:
-# - Time coarse/fine buttons
+obj = CR3BP() 
 
 # The parametrized function to be plotted
 def f(tf, x0, y0, z0, vx0, vy0, vz0):
-    t, states = obj.propagate_orbit([x0, y0, z0, vx0, vy0, vz0], tf)
+    _, states = obj.propagate_orbit([x0, y0, z0, vx0, vy0, vz0], tf)
     return states[:, :3]
 
-# Define initial parameters
-x0 = 0.75; y0 = 0; z0 = 0; vx0 = 0; vy0 = 0.5; vz0 = 0; tf = 5
+state_vars = ["tf", "x0", "y0", "z0", "vx0", "vy0", "vz0"]
+# value, slidermin, slidermax
+defaultvals = {"tf": [3, 0, 10], "x0": [0.82, -1, 1], "y0": [0, -1, 1], "z0": [0.02, -1, 1], "vx0": [0, -0.5, 0.5], "vy0": [0.15, -0.5, 0.5], "vz0": [0, -0.5, 0.5]}
 
 # Create the figure and the line that we will manipulate
 fig = plt.figure()
@@ -38,110 +36,159 @@ for n, axis in enumerate([ax.yaxis, ax.zaxis, ax.xaxis]):
     axis.pane.set_edgecolor([0.75,0.75,0.75,0.5])
     axis.gridlines.set_alpha(0.25)
 ax.set_axis_off()
-
-
 bodies = ax.plot([-obj.mu, 1 - obj.mu], [0, 0], "co", markersize=4)
 lagrange_points = ax.plot(obj.L_points[0, :], obj.L_points[1, :], "wo", markersize=2, visible=False)
-
-traj = ax.plot(*f(tf, x0, y0, z0, vx0, vy0, vz0).T, "r", lw=1)
-
+traj = ax.plot(*f(*[defaultvals[var][0] for var in state_vars]).T, "r", lw=1)
 plt.axis("equal")
 
-tf_axis = fig.add_axes([0.05, 0.1, 0.01, 0.85])
-tf_slider = Slider(ax=tf_axis, label="$t_f$", valmin=0, valmax=15, valinit=tf, orientation="vertical", color=[.3,.3,.3], track_color=[.1,.1,.1])
-
-
-slider_vars = ["x0", "y0", "z0", "vx0", "vy0", "vz0"]
 slider_objs = []
 slider_axes = []
-num_sliders = len(slider_vars)
+num_sliders = len(state_vars)
 
-for n, varName in enumerate(slider_vars):
-    label_firstletter = varName[0]
-    label_rest = varName[1:]
+for n, varname in enumerate(state_vars):
+    label_firstletter = varname[0]
+    label_rest = varname[1:]
     
     slider_spacing = 0.04
     margin = 0.025
     
+    slider_vals = defaultvals[varname]
+
     slider_ax = fig.add_axes([1-margin-(num_sliders-n-1)*slider_spacing, 0.1, 0.01, 0.85])
     slider_axes.append(slider_ax)
     slider_obj = Slider(ax=slider_axes[-1],
                         label="$"+label_firstletter+"_{"+label_rest+"}$",
-                        valmin=-1.25, valmax=1.25, valinit=globals()[varName],
-                        orientation="vertical", color=[.3,.3,.3], track_color=[.1,.1,.1])
+                        valmin=slider_vals[1], valmax=slider_vals[2], valinit=slider_vals[0],
+                        orientation="vertical", track_color=[.25,.25,.25])
+    slider_obj.poly.set_visible(False)
     slider_objs.append(slider_obj)
 
+# buttons
+label_ax = fig.add_axes([0.65, 0.7, 0.06, 0.04])
+label_ax.text(0.5,0.5, "Sliders:",verticalalignment='center', horizontalalignment='center')
+label_ax.set_facecolor([.1,.1,.1,0])
+label_ax.set_xticks([])
+label_ax.set_yticks([])
+
+reset_ax = fig.add_axes([0.65, 0.65, 0.06, 0.04])
+reset_btn = Button(reset_ax, 'Reset', hovercolor='0.975', color='0.25')
+
+zoomout_ax = fig.add_axes([0.65, 0.6, 0.06, 0.04])
+zoomout_btn = Button(zoomout_ax, 'Coarser', hovercolor='0.975', color='0.25')
+
+zoomin_ax = fig.add_axes([0.65, 0.55, 0.06, 0.04])
+zoomin_btn = Button(zoomin_ax, 'Finer', hovercolor='0.975', color='0.25')
+
+center_ax = fig.add_axes([0.65, 0.5, 0.06, 0.04])
+center_btn = Button(center_ax, 'Center', hovercolor='0.975', color='0.25')
+
+
+label_ax = fig.add_axes([0.025, 0.7, 0.075, 0.04])
+label_ax.text(0.5,0.5, "Toggle:",verticalalignment='center', horizontalalignment='center')
+label_ax.set_facecolor([.1,.1,.1,0])
+label_ax.set_xticks([])
+label_ax.set_yticks([])
+lagrange_ax = fig.add_axes([0.025, 0.6, 0.075, 0.04])
+lagrange_btn = Button(lagrange_ax, 'Lagrange Pts', hovercolor='0.975', color='0.25')
+
+axtoggle_ax = fig.add_axes([0.025, 0.65, 0.075, 0.04])
+axtoggle_btn = Button(axtoggle_ax, 'Axes Type', hovercolor='0.975', color='0.25')
+
+
+# Optimizer
+
+label_ax = fig.add_axes([0.025, 0.4, 0.075, 0.04])
+label_ax.text(0.5,0.5, "Optimization:",verticalalignment='center', horizontalalignment='center')
+label_ax.set_facecolor([.1,.1,.1,0])
+label_ax.set_xticks([])
+label_ax.set_yticks([])
+optimize_ax = fig.add_axes([0.025, 0.16, 0.075, 0.04])
+optimize_btn = Button(optimize_ax, 'Optimize', hovercolor='0.975', color='0.25')
+
+
+obj_zero=["y", "vx", "vz"]
+opt_vars=["tf", "x", "vy"]
+
+optvar_ax = fig.add_axes([0.025, 0.3, 0.075, 0.1])
+optvar_btns = CheckButtons(
+    ax=optvar_ax,
+    labels=[var+" free" for var in state_vars[1:]],
+    actives=[var[:-1] in opt_vars for var in state_vars[1:]],
+    label_props={'color': 'w'},
+    frame_props={'edgecolor': 'w'},
+    check_props={'facecolor': 'w'},
+)
+def toggle_optvar(label):
+    varname = label.split()[0][:-1]
+    if varname in opt_vars: opt_vars.remove(varname)
+    else: opt_vars.append(varname)
+optvar_btns.on_clicked(toggle_optvar)
+
+objzero_ax = fig.add_axes([0.025, 0.2, 0.075, 0.1])
+objzero_btns = CheckButtons(
+    ax=objzero_ax,
+    labels=[var[:-1]+"f=0" for var in state_vars[1:]],
+    actives=[var[:-1] in obj_zero for var in state_vars[1:]],
+    label_props={'color': 'w'},
+    frame_props={'edgecolor': 'w'},
+    check_props={'facecolor': 'w'},
+)
+def toggle_objzero(label):
+    varname = label.split("=")[0][:-1]
+    if varname in obj_zero: obj_zero.remove(varname)
+    else: obj_zero.append(varname)
+objzero_btns.on_clicked(toggle_objzero)
 
 # The function to be called anytime a slider's value changes
 def update(val):
-    states = f(tf_slider.val, *[slider.val for slider in slider_objs])
+    states = f(*[slider.val for slider in slider_objs])
     traj[0].set_data_3d(states[:, 0], states[:, 1], states[:, 2])
     fig.canvas.draw_idle()
 
-# register the update function with each slider
-for slider in [*slider_objs, tf_slider]:
-    slider.on_changed(update)
-
-# buttons
-periodic_ax = fig.add_axes([0.7, 0.25, 0.06, 0.04])
-periodic_btn = Button(periodic_ax, 'Close Orbit', hovercolor='0.975', color='0.25')
-
-center_ax = fig.add_axes([0.7, 0.4, 0.06, 0.04])
-center_btn = Button(center_ax, 'Center', hovercolor='0.975', color='0.25')
-
-zoomin_ax = fig.add_axes([0.7, 0.5, 0.06, 0.04])
-zoomin_btn = Button(zoomin_ax, 'Finer', hovercolor='0.975', color='0.25')
-
-zoomout_ax = fig.add_axes([0.7, 0.6, 0.06, 0.04])
-zoomout_btn = Button(zoomout_ax, 'Coarser', hovercolor='0.975', color='0.25')
-
-reset_ax = fig.add_axes([0.7, 0.7, 0.06, 0.04])
-reset_btn = Button(reset_ax, 'Reset', hovercolor='0.975', color='0.25')
-
-axtoggle_ax = fig.add_axes([0.075, 0.6, 0.06, 0.04])
-axtoggle_btn = Button(axtoggle_ax, 'Axes', hovercolor='0.975', color='0.25')
-
-lagrange_ax = fig.add_axes([0.075, 0.4, 0.075, 0.04])
-lagrange_btn = Button(lagrange_ax, 'Lagrange Pts', hovercolor='0.975', color='0.25')
-
 def update_sliders(zoom=None):
-    for num in range(len(slider_objs)):
-        old_valmin = slider_objs[num].valmin
-        old_valmax = slider_objs[num].valmax
-        curr_val = slider_objs[num].val
-        val_range = old_valmax-old_valmin
-        new_valmax = (curr_val + zoom*val_range) if zoom is not None else 1
-        new_valmin = (curr_val - zoom*val_range) if zoom is not None else -1
-        new_valinit = curr_val if zoom is not None else globals()[slider_vars[num]]
+    for slider in slider_objs:
+        varname = ''.join(ch for ch in slider.label._text if ch.isalnum())
         
-        slider_ax = fig.add_axes([1-margin-(num_sliders-num-1)*slider_spacing, 0.1, 0.01, 0.85])
-        slider_objs[num].ax.remove()
-        slider_obj = Slider(ax=slider_ax,
-                            label=slider_objs[num].label._text,
-                            valmin=new_valmin, valmax=new_valmax, valinit=new_valinit,
-                            orientation=slider_objs[num].orientation,
-                            track_color=slider_objs[num].track._facecolor,
-                            color=slider_objs[num].poly._facecolor)
-        slider_objs[num] = slider_obj
-        for slider in [*slider_objs, tf_slider]:
-            slider.on_changed(update)
+        slider_vals = defaultvals[varname]
+        
+        if (varname == "tf") and zoom is not None:
+            if zoom<1:
+                zoom *= 5
+            else:
+                zoom /= 5
+            # time zoom should be much less sensative
+        
+        old_valmin = slider.valmin
+        old_valmax = slider.valmax
+        curr_val = slider.val
+        val_range = old_valmax-old_valmin
+        new_valmax = (curr_val + 0.5*zoom*val_range) if zoom is not None else slider_vals[2]
+        new_valmin = (curr_val - 0.5*zoom*val_range) if zoom is not None else slider_vals[1]
+        new_valinit = curr_val if zoom is not None else slider_vals[0]
+        
+        
+        slider.ax.set_ylim(new_valmin,new_valmax)
+        slider.valmin = new_valmin
+        slider.valmax = new_valmax
+        for slider2 in slider_objs:
+            if slider2 != slider: slider2.eventson = False
+        slider.set_val(new_valinit)
+        for slider2 in slider_objs:
+            if slider2 != slider: slider2.eventson = True
+        fig.canvas.draw_idle()
 
 def reset(event):
     update_sliders()
     fig.canvas.draw_idle()
-reset_btn.on_clicked(reset)
 
 def center(event):
     update_sliders(zoom=1)
-center_btn.on_clicked(center)
 
 def zoomin(event):
     update_sliders(zoom=0.1)
-zoomin_btn.on_clicked(zoomin)
 
 def zoomout(event):
     update_sliders(zoom=10)
-zoomout_btn.on_clicked(zoomout)
 
 def swap_axes(event):
     if xy_plane.get_visible():
@@ -157,28 +204,38 @@ def swap_axes(event):
         xyz_axes.set_visible(True)
         ax.set_axis_off()
     fig.canvas.draw_idle()
-axtoggle_btn.on_clicked(swap_axes)
-
 
 def toggle_Lpoints(event):
     lagrange_points[0].set_visible(not lagrange_points[0].get_visible())
     fig.canvas.draw_idle()
-lagrange_btn.on_clicked(toggle_Lpoints)
-
 
 def make_periodic(event):
-    curr_state = [tf_slider.val, *[slider.val for slider in slider_objs]]
-    new_state = obj.find_periodic_orbit(opt_vars=["tf", "z", "vy"], obj_zero=["vx", "y"], init_guess=curr_state, tol=1e-10)
+    curr_state = [slider.val for slider in slider_objs]
+    print(opt_vars)
+    print(obj_zero)
+    if len(obj_zero)>0:
+        new_state = obj.find_periodic_orbit(opt_vars=opt_vars, obj_zero=obj_zero, init_guess=curr_state, tol=1e-10)
+    else:
+        new_state = curr_state
     
-    for n, slider in enumerate([tf_slider, *slider_objs]):
+    for n, slider in enumerate(slider_objs):
         # eventson = False so that there isn't an infinite loop
-        for slider2 in [tf_slider, *slider_objs]:
+        for slider2 in slider_objs:
             if slider2 != slider: slider2.eventson = False
         slider.set_val(new_state[n])
-        for slider2 in [tf_slider, *slider_objs]:
+        for slider2 in slider_objs:
             if slider2 != slider: slider2.eventson = True
-periodic_btn.on_clicked(make_periodic)
+
+reset_btn.on_clicked(reset)
+center_btn.on_clicked(center)
+zoomin_btn.on_clicked(zoomin)
+zoomout_btn.on_clicked(zoomout)
+axtoggle_btn.on_clicked(swap_axes)
+lagrange_btn.on_clicked(toggle_Lpoints)
+optimize_btn.on_clicked(make_periodic)
+for slider in slider_objs:
+    slider.on_changed(update)
 
 # adjust the main plot to make room for the sliders
-fig.subplots_adjust(right=.75, left=0.075, bottom=0.025, top=0.975)
+fig.subplots_adjust(right=.7, left=0.025, bottom=0.025, top=0.975)
 plt.show()
