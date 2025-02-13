fc3f2358d9069d3fe6e274eb8c31b6de04d90376
Polaris <imll1@icloud.com>
2025-02-12 19:11:41 -0500

    added plots and some code

--- Code/CR3BP.py
+++ Code/CR3BP.py
@@ -45,6 +45,32 @@ class CR3BP:
 
         return Ugrav + Ucent
 
+    def U_hess(self, r):
+        D = r - np.array([-self.mu, 0, 0])
+        R = r - np.array([1 - self.mu, 0, 0])
+        H = (
+            np.diag([1, 1, 0])
+            + 3 * (1 - self.mu) / np.linalg.norm(D) ** 5 * np.outer(D, D)
+            - (1 - self.mu) / np.linalg.norm(D) ** 3 * np.eye(3)
+            + 3 * self.mu / np.linalg.norm(R) ** 5 * np.outer(R, R)
+            - self.mu / np.linalg.norm(R) ** 3 * np.eye(3)
+        )
+        return H
+
+    def get_A(self,state):
+        r = state[:3]
+        Uxx = self.U_hess(r)
+        O = np.zeros((3, 3))
+        I = np.eye(3)
+        Omega = np.array([[0, 2, 0], [-2, 0, 0], [0, 0, 0]])
+        A = np.block([[O, I], [Uxx, Omega]])
+        return A
+    
+    def get_JC(self, x, y, z, dx=0, dy=0, dz=0):
+        JC = 2 * self.pseudopotential(x, y, z)
+        JC -= dx**2 + dy**2 + dz**2
+        return JC
+
     def eom(self, t, state):
         x, y, z, vx, vy, vz = state
         xyz = state[:3]
@@ -141,7 +167,6 @@ class CR3BP:
         plt.grid(linestyle="dashed", lw=0.5, c="gray")
         plt.show()
 
-    # TODO: Tolerance specification, geq and leq constraints, multiple fixed ICs?
     def find_periodic_orbit(self,
                             opt_vars=["tf", "z", "vy"],
                             obj_zero=["vx", "y"],
@@ -176,3 +201,152 @@ class CR3BP:
         optimal_state[func_inputs[fixed_vars]] = init_guess[func_inputs[fixed_vars]]
         optimal_state[func_inputs[opt_vars]] = minimizing_guess
         return optimal_state
+    
+    def coupled_stm_eom(self, t, state):
+        pv = state[:6]
+        dpv = self.eom(t, pv)
+        stm = state[6:].reshape((6, 6))
+        A = self.get_A(pv)  # pv[:3]
+        dstm = A @ stm
+
+        dstate = np.array([*dpv, *dstm.flatten()])
+        return dstate
+    
+    def targetter_planar_xfixed(self, X0, tol=1e-10, int_tol=1e-6, max_iters=50):
+
+        terminate = lambda t, x: x[1]
+        terminate.terminal = True
+
+        trajs_stms = []
+        ics = [X0]
+        corrections = []
+
+        err = np.inf
+        while np.linalg.norm(err) > tol:
+            xstmIC = np.array([*X0, *np.eye(6).flatten()])
+            terminate.direction = -int(np.sign(X0[4]))
+            soln = solve_ivp(
+                self.coupled_stm_eom,
+                (0, np.inf),
+                xstmIC,
+                events=terminate,
+                rtol=int_tol,
+                atol=int_tol / 100,
+            )
+
+            # no clue why double 0 is needed, but it is
+            Xf, STM = soln.y_events[0][0][:6], soln.y_events[0][0][6:].reshape(6, 6)
+
+            ddx = self.eom(0, Xf)[-3]
+            dy = Xf[4]
+            dx = Xf[3]
+            correction = -dx / (STM[3, 4] - STM[1, 4] * ddx / dy)
+            err = dx
+
+            corrections.append(correction)
+            X0 += np.array([0, 0, 0, 0, correction, 0])
+            ics.append(X0)
+            trajs_stms.append(soln.y)
+
+        return X0, trajs_stms, ics, corrections
+
+
+    def targetter_zfixed(self, X0, tol=1e-10, int_tol=1e-6, max_iters = 50):
+
+        # X0 = ref + np.array([dx0, 0, 0, 0, dvy0, 0])
+        terminate = lambda t, x: x[1]
+        terminate.terminal = True
+
+        trajs_stms = []
+        ics = [X0]
+        corrections = []
+
+        err = np.inf
+        while np.linalg.norm(err) > tol:
+            xstmIC = np.array([*X0, *np.eye(6).flatten()])
+            terminate.direction = -int(np.sign(X0[4]))
+
+            soln = solve_ivp(
+                self.coupled_stm_eom,
+                (0, np.inf),
+                xstmIC,
+                events=terminate,
+                rtol=int_tol,
+                atol=int_tol / 100,
+            )
+
+            # no clue why double 0 is needed, but it is
+            Xf, STM = soln.y_events[0][0][:6], soln.y_events[0][0][6:].reshape(6, 6)
+
+            ddx = self.eom(0, Xf)[-3]
+            ddz = self.eom(0, Xf)[-1]
+            dx = Xf[-3]
+            dy = Xf[-2]
+            dz = Xf[-1]
+            mtx = (
+                np.array([[STM[3, 0], STM[3, 4]], [STM[5, 0], STM[5, 4]]])
+                - np.outer([ddx, ddz], [STM[1, 0], STM[1, 4]]) / dy
+            )
+            mtxInv = np.array([[mtx[1, 1], -mtx[0, 1]], [-mtx[1, 0], mtx[0, 0]]]) / (
+                mtx[0, 0] * mtx[1, 1] - mtx[0, 1] * mtx[1, 0]
+            )
+            correction = -mtxInv @ np.array([dx, dz])
+            err = np.linalg.norm([dx, dz])
+
+            corrections.append(correction)
+
+            X0 += np.array([correction[0], 0, 0, 0, correction[1], 0])
+            ics.append(X0)
+            trajs_stms.append(soln.y)
+
+        return X0, trajs_stms, ics, corrections
+
+
+    def targetter_xfixed(self, X0, tol=1e-10, int_tol=1e-6, max_iters = 50):
+
+        terminate = lambda t, x: x[1]
+        terminate.terminal = True
+
+        trajs_stms = []
+        ics = [X0]
+        corrections = []
+
+        err = np.inf
+        while np.abs(err) > tol:
+            xstmIC = np.array([*X0, *np.eye(6).flatten()])
+            terminate.direction = -int(np.sign(X0[4]))
+
+            soln = solve_ivp(
+                self.coupled_stm_eom,
+                (0, np.inf),
+                xstmIC,
+                events=terminate,
+                rtol=int_tol,
+                atol=int_tol / 100,
+            )
+
+            # no clue why double 0 is needed, but it is
+            Xf, STM = soln.y_events[0][0][:6], soln.y_events[0][0][6:].reshape(6, 6)
+
+            ddx = self.eom(0, Xf)[-3]
+            ddz = self.eom(0, Xf)[-1]
+            dx = Xf[-3]
+            dy = Xf[-2]
+            dz = Xf[-1]
+            mtx = (
+                np.array([[STM[3, 2], STM[3, 4]], [STM[5, 2], STM[5, 4]]])
+                - np.outer([ddx, ddz], [STM[1, 2], STM[1, 4]]) / dy
+            )
+            mtxInv = np.array([[mtx[1, 1], -mtx[0, 1]], [-mtx[1, 0], mtx[0, 0]]]) / (
+                mtx[0, 0] * mtx[1, 1] - mtx[0, 1] * mtx[1, 0]
+            )
+            correction = -mtxInv @ np.array([dx, dz])
+            err = np.linalg.norm([dx, dz])
+
+            corrections.append(correction)
+
+            X0 += np.array([0, 0, correction[0], 0, correction[1], 0])
+            ics.append(X0)
+            trajs_stms.append(soln.y)
+
+        return X0, trajs_stms, ics, corrections
